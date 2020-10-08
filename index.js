d2ec28c70c99e3c613ee976ae68ef48b886ac95d
SergeyYakimov <teriertis@yandex.ru>
2020-10-09 01:27:45 +0800

    Realized select with backdrop

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ import {Select} from './select/select'
 import './select/style.scss'
 
 const select = new Select('#select', {
-
+  placeholder: 'Выберите, пожалуйста, элемент',
+  selectedId: '3',
+  data: [
+    {id: '1', value: 'Angular'},
+    {id: '2', value: 'React'},
+    {id: '3', value: 'Vue'},
+    {id: '4', value: 'React Native'},
+    {id: '5', value: 'Nuxt'},
+    {id: '6', value: 'Next'}
+  ],
+  onSelect(item) {
+    console.log('Selected item', item)
+  }
 })
 
 window.s = select
--- select/select.js
+++ select/select.js
@@ -1,13 +1,29 @@
-const getTemplate = () => {
+const getTemplate = (data = [], placeholder, selectedId) => {
+  let text = placeholder ?? 'Пожалуйста, выберите вариант'
+
+  const items = data.map(item => {
+    let cls = ''
+
+    if (item.id === selectedId) {
+      text = item.value
+      cls = 'selected'
+    }
+
+    return ` 
+       <li class="select__item ${cls}" data-type="item" data-value="${item.id}">${item.value}</li>
+      `
+  }).join('')
+
   return `
+  <div class="select__backdrop" data-type="backdrop">
+  </div>
   <div class="select__input" data-type="input">
-      <span>Text</span>
+      <span data-type="text">${text}</span>
       <i class="fa fa-chevron-down" data-type="arrow"></i>
   </div>
   <div class="select__dropdown">
       <ul class="select__list">
-          <li class="select__item">1</li>
-          <li class="select__item">1112</li>
+         ${items}
       </ul>
   </div>
   `
@@ -16,20 +32,24 @@ const getTemplate = () => {
 export class Select {
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
+    this.options = options
+    this.selectedId = options.selectedId
 
     this.#render()
     this.#setup()
   }
 
   #render() {
+    const {placeholder, data} = this.options
     this.$el.classList.add('select')
-    this.$el.innerHTML = getTemplate()
+    this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId)
   }
 
   #setup() {
     this.clickHandler = this.clickHandler.bind(this)
     this.$el.addEventListener('click', this.clickHandler)
     this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+    this.$text = this.$el.querySelector('[data-type="text"]')
   }
 
   clickHandler(event) {
@@ -37,7 +57,30 @@ export class Select {
 
     if (type === 'input') {
       this.toggle()
+    } else if (type === 'item') {
+      const id = event.target.dataset.value
+      this.select(id)
+    } else if (type === 'backdrop') {
+      this.close()
     }
+  }
+
+  get current() {
+    return this.options.data.find(item => item.id === this.selectedId)
+  }
+
+  select(id) {
+    this.selectedId = id
+    this.$text.textContent = this.current.value
+
+    this.$el.querySelectorAll('[data-type="item"]').forEach(item => {
+      item.classList.remove('selected')
+    })
+    this.$el.querySelector(`[data-value="${id}"]`).classList.add('selected')
+
+    this.options.onSelect ? this.options.onSelect(this.current) : null
+
+    this.close()
   }
 
   get isOpen() {
@@ -62,5 +105,6 @@ export class Select {
 
   destroy() {
     this.$el.removeEventListener('click', this.clickHandler)
+    this.$el.innerHTML = ''
   }
 }
